113dcdda7547ce67c395381ae4ad689a699b3b0e
Andrew Smith <runtheexe@gmail.com>
2014-04-02 18:31:55 -0400

    Ion Auth
    Refreshingly simple.

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = ':Y7FmXg2%&KUL?\0X\TdoNOsTo`sV.[,';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/dash.php
+++ application/controllers/dash.php
@@ -7,16 +7,31 @@ class Dash extends CI_Controller {
         parent::__construct();
         $this->load->model('restaurant_model');
         $this->load->helper('url');
+        $this->load->library('ion_auth');
     }
 
     public function index()
     {
+        $this->authorize();
         $this->load->view('dash/index');
     }
 
     public function restaurants()
     {
+        $this->authorize();
         $data['restaurants'] = $this->restaurant_model->get_restaurants();
         $this->load->view('dash/restaurants', $data);
+    }
+
+    private function authorize()
+    {
+        if (!$this->ion_auth->logged_in())
+        {
+            redirect('auth/login', 'refresh');
+        }
+        elseif (!$this->ion_auth->is_admin())
+        {
+            return show_error('You must be an administrator to view this page.');
+        }
     }
 }
